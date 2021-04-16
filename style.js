0e0375f9defcdb739c6ea405bb1e27fe5c8ab751
arjunMee <70093722+arjunMee@users.noreply.github.com>
2021-04-16 23:12:32 +0530

    f

--- style.js
+++ style.js
@@ -2,54 +2,69 @@
 // .then(rep => rep.json())
 // .then(pro => console.log(pro));
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify({
-        title: 'foo',
-        body: 'bar',
-        userId: 1,
-    }),
-    header: {
-        'content-type': 'application/json',
-    },
-})
 
-.then(rep => rep.json())
-.then(pro => console.log(pro));
+
+const wait = time => new Promise( res => setTimeout(() => res(), time);
+
+wait(200)
+.then(()=> new Promise(res => res('foo')))
 
 
 
 
-fetch('https://jsonplaceholder.typicode.com/posts/5', {
-    method: 'PUT',
+
+
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         title: 'foo',
+//         body: 'bar',
+//         userId: 1,
+//     }),
+//     header: {
+//         'content-type': 'application/json',
+//     },
+// })
+
+// .then(rep => rep.json())
+// .then(pro => console.log(pro));
+
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts/5', {
+//     method: 'PUT',
     
-    body: JSON.stringify({
-        userId: 1,
-        id: 5,
-        title: "hello task",
-        completed: false,
-        body: "loda lasan ppppp"
-    }),
-    headers: {
-    "Content-type": "application/json"
-    }
-})
+//     body: JSON.stringify({
+//         userId: 1,
+//         id: 5,
+//         title: "hello task",
+//         completed: false,
+//         body: "loda lasan ppppp"
+//     }),
+//     headers: {
+//     "Content-type": "application/json"
+//     }
+// })
 
-.then(rep => rep.json())
-.then(pro => console.log(pro));
+// .then(rep => rep.json())
+// .then(pro => console.log(pro));
 
 
-fetch('https://jsonplaceholder.typicode.com/posts/5', {
-    method: 'PATCH',
+// fetch('https://jsonplaceholder.typicode.com/posts/5', {
+//     method: 'POST',
     
-    body: JSON.stringify({
-        body: "loda lasan"
-    }),
-    headers: {
-    "Content-type": "application/json"
-    }
-})
+//     body: JSON.stringify({
+//         userId
+//     }),
+//     headers: {
+//     "Content-type": "application/json"
+//     }
+// })
 
-.then(rep => rep.json())
-.then(pro => console.log(pro));
+// .then(rep => rep.json())
+// .then(pro => console.log(pro));
 
